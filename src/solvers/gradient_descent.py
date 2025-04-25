6ad9dce4521e9660fd003d3794596e0591d89a2e
lavrentious <lavrent0@yandex.ru>
2025-04-25 10:41:21 +0300

    fix: iteration count

--- src/solvers/gradient_descent.py
+++ src/solvers/gradient_descent.py
@@ -5,7 +5,7 @@ import sympy as sp  # type: ignore
 from config import GD_STEP
 from utils import compute_fn
 
-MAX_ITERATIONS = 100
+MAX_ITERATIONS = 10000
 
 
 def gradient_descent(
@@ -19,6 +19,7 @@ def gradient_descent(
     x0 - starting point vector
     dfxs - list of corresponding partial derivatives
     """
+    step = GD_STEP
     x1_symbol, x2_symbol = sp.symbols("x1, x2")
     M = list(x0)
     grad = [sp.diff(fn.expr, x1_symbol), sp.diff(fn.expr, x2_symbol)]
@@ -31,10 +32,12 @@ def gradient_descent(
             print(f"iteration={i+1}: {M=}")
 
         M = [
-            M[0] - GD_STEP * grad[0].subs({x1_symbol: M[0], x2_symbol: M[1]}).evalf(),
-            M[1] - GD_STEP * grad[1].subs({x1_symbol: M[0], x2_symbol: M[1]}).evalf(),
+            M[0] - step * grad[0].subs({x1_symbol: M[0], x2_symbol: M[1]}).evalf(),
+            M[1] - step * grad[1].subs({x1_symbol: M[0], x2_symbol: M[1]}).evalf(),
         ]
         new_f = compute_fn(fn, M[0], M[1])
+        if new_f >= prev_f and prev_f != -sp.oo:
+            step /= 2
         if verbose:
             print(f"-new M: {M}, f(M)={compute_fn(fn, M[0], M[1])}")
 
